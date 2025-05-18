c37125a9c0c2e8bcf7e9898b2831baa642681c6b
samuel <samuelpc4567@gmail.com>
2025-05-18 10:01:45 -0300

    Adicionando criptografia

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Inicializar o Prisma
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('API QuadraLivre funcionando!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
