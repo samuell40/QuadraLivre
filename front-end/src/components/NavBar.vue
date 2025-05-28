ad30c28d5bcf57a06c391c8035cf0abebefe7f78
samuel <samuelpc4567@gmail.com>
2025-05-28 15:02:29 -0300

    Adicionando responsividade ao NAV BAR

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -3,7 +3,14 @@
     <nav class="navbar-custom">
       <div class="navbar-container">
         <div class="logo">Quadra Livre</div>
-        <ul class="nav-links">
+
+        <div class="hamburger" @click="toggleMenu">
+          <span :class="{ open: isMenuOpen }"></span>
+          <span :class="{ open: isMenuOpen }"></span>
+          <span :class="{ open: isMenuOpen }"></span>
+        </div>
+
+        <ul class="nav-links" :class="{ active: isMenuOpen }">
           <li><a href="/quadra">Quadras</a></li>
           <li><a href="/horarios">Horários</a></li>
           <li><a href="/placar">Placar</a></li>
@@ -17,28 +24,37 @@
 <script>
 export default {
   name: 'NavBar',
-}
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .navbar-custom {
   position: fixed;
-  display: flex;
   top: 0;
   left: 0;
   width: 100%;
   background-color: #152147;
   height: 70px;
   font-family: "Montserrat", sans-serif;
+  z-index: 1000;
 }
 
 .navbar-container {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 100%;
   padding: 0 30px;
-  flex-wrap: wrap;
+  height: 100%;
 }
 
 .logo {
@@ -47,6 +63,7 @@ export default {
   white-space: nowrap;
 }
 
+/* Links */
 .nav-links {
   list-style: none;
   display: flex;
@@ -65,16 +82,43 @@ export default {
   background-color: #1E3A8A;
   padding: 6px 16px;
   border-radius: 30px;
-  font-weight:"Montserrat";
 }
 
-.image-container {
-  margin-top: 70px;
+/* Hamburger */
+.hamburger {
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  gap: 5px;
 }
 
-.image-container img {
+.hamburger span {
+  width: 25px;
+  height: 3px;
+  background-color: #fff;
+  transition: 0.3s;
+}
+
+/* Menu mobile ativo */
+.nav-links.active {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 70px;
+  right: 0;
+  background-color: #152147;
   width: 100%;
-  height: auto;
-  display: block;
+  padding: 20px 0;
+  gap: 20px;
+}
+
+@media (max-width: 768px) {
+  .nav-links {
+    display: none;
+  }
+
+  .hamburger {
+    display: flex;
+  }
 }
 </style>
