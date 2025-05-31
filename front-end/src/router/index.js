187cbf437bde0f0ea6a9c6a1ad3764b12b51a9a4
João Pedro Barros <jao.pepedro14@gmail.com>
2025-05-31 18:23:15 -0300

    Correção de um erro breve no index.js

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AgendamentoView from '../views/AgendamentoView.vue';
 import LoginView from '../views/LoginView.vue';
-import CadastrarQuadraView from '../views/CadastrarQuadraView.vue'
+import CadastrarQuadraView from '../views/CadastrarQuadraView.vue';
 
 const routes = [
   {
@@ -10,8 +10,7 @@ const routes = [
     name: 'Home',
     component: HomeView,
   },
-
-    {
+  {
     path: '/agendamento',
     name: 'Agendamento',
     component: AgendamentoView,
@@ -20,6 +19,8 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: LoginView,
+  },
+  {
     path: '/cadastrarquadra',
     name: 'cadastrar_quadra',
     component: CadastrarQuadraView,
